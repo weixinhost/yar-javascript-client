a467fa35b8a3e4078e4a3e5d07e5e94b0649be22
Misko_Lee <imiskolee@gmail.com>
2014-12-27 23:26:06 +0800

    将Struct内存布局进行了重新描述

--- src/yar.js
+++ src/yar.js
@@ -1,3 +1,4 @@
+
 (function(window,undefined){
 
     function yar_exception(message){
@@ -8,19 +9,29 @@
         return 1000000 + (parseInt(Math.random() * 10000000));
     }
 
+    function str_byte_length(str){
+        var doubleByteChars = str.match(/[^\x00-\xfe]/ig);
+        return str.length + (doubleByteChars == null ? 0 : doubleByteChars.length);
+    }
+
     function Yar_Header(){
 
-        /*****   Yar Protocol *****/
-        this.id             = 0;            //Uint32
-        this.version        = 1;            //Uint16
-        this.magic_number   = 0x80DFEC60;   //Uint32
-        this.reserved       = 0;            //Uint32
-        this.provider       = '';           //Uint8 32 elements
-        this.token          = '';           //Uint8 32 elements
-        this.body_len       = 0;            //request_body len
-        this.pack           = 'JSON';
-        this._buffer        = null;   //ArrayBuffer
-        this._view          = null;   //DataView
+        this.struct = new CStruct.CStruct(
+            {
+                id              : new CStruct.CVal('uint32',0),
+                version         : new CStruct.CVal('uint16',1),
+                magic_number    : new CStruct.CVal('uint32',0x80DFEC60),
+                reserved        : new CStruct.CVal('uint32',0),
+                provider        : new CStruct.CArray(32),
+                token           : new CStruct.CArray(32),
+                body_len        : new CStruct.CVal('uint32',0),
+                pack            : new CStruct.CVal('string','JSON\0\0\0\0'),
+                body            : new CStruct.CVal('string','')
+            },
+            ['id','version','magic_number','reserved','provider','token','body_len','pack','body']
+        );
+
+
 
     }
 
@@ -28,39 +39,10 @@
         if('object' == typeof body){
             body = JSON.stringify(body);
         }
-        this.body_len = body.length;
-        var strView = new StringView(body,'',0,body.length);
-
-        this.body_len =2000;
-        this._buffer = new ArrayBuffer(90 + strView.bufferView.byteLength);
-        this._view = new DataView(this._buffer);
-
-        this._view.setUint32(0,this.id);
-        this._view.setUint16(4,this.version);
-        this._view.setUint32(6,this.magic_number);
-        this._view.setUint32(10,this.reserved);
-        for(var i=0;i<32;i++){
-            this._view.setUint8(14+i,0);
-        }
-        for(var i=0;i<32;i++){
-            this._view.setUint8(46+i,0);
-        }
-         this._view.setUint32(78,this.body_len);
-        var pack = 'JSON';
-        this._view.setUint8(82,pack.charCodeAt(0));
-        this._view.setUint8(83,pack.charCodeAt(1));
-        this._view.setUint8(84,pack.charCodeAt(2));
-        this._view.setUint8(85,pack.charCodeAt(3));
-        this._view.setUint8(86,0);
-        this._view.setUint8(87,0);
-        this._view.setUint8(88,0);
-        this._view.setUint8(89,0);
-
-        var strView = new StringView(body,'',0,body.length);
-        for(var i =0;i<strView.bufferView.byteLength;i++){
-            this._view.setUint8(90 +i,strView.bufferView[i]);
-        }
-
+        this.struct.get('body_len').val = body.length;
+        this.struct.get('body').val = body;
+        this.struct.get('id').val =123456789;
+        return this.struct.raw();
     }
 
     function YarClient(server){
@@ -69,13 +51,13 @@
 
     YarClient.prototype.call = function(method,parmters,callback){
         var _request = {
-            i:random_id(),
+            i:123456789,
             'm':(''+method),
             'p':parmters || []
         };
         var _header = new Yar_Header();
         _header.id = _request['id'];
-        _header.create(_request);
+        var _send = _header.create(_request);
         var exchange=new XMLHttpRequest();
         exchange.onreadystatechange = function () {
             if (exchange.readyState == 4) {
@@ -94,7 +76,7 @@
             }
         }
         exchange.open("POST",this.address,true);
-        exchange.send(_header._view);
+        exchange.send(_send);
     }
 
     window.YarClient = YarClient;
